257e1b2c6337880c3e38c25680d0ea64a30ef4b8
oasisdemo <oasisdemo@sina.cn>
2016-10-15 11:35:02 +0800

    auto

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
 @rem commit.bat
 @rem set path=%path%;"D:\\Program Files (x86)\\Git\\bin"
 git add --all
-git commit -m "Initial commit"
+git commit -m "auto"
 git push -u origin master
-pause
+@rem pause
